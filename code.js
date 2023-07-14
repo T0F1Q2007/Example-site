6c0eb5f1a122dcf0bedc168c0eac1023b27f6ad4
T0F1Q2007 <79853089+T0F1Q2007@users.noreply.github.com>
2023-07-14 13:21:48 +0400

    Add files via upload

--- code.js
+++ code.js
@@ -58,3 +58,9 @@ function checkInput(event) {
     }
   }
 }
+document.cookie = "Cookie=chocolate";
+var date = new Date();
+date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+var expires = "expires=" + date.toUTCString();
+
+document.cookie = "Cookie=chocolate; " + expires;
